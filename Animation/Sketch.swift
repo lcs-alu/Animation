da0469f3f29fb20f328e6638428287b816e8c305
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2017-10-27 12:56:23 -0400

    five circles

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -26,9 +26,30 @@ class Sketch : NSObject {
         // Change position
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
+        // Draw an ellipse in the middle of the canvas.right
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.purple
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
+        // Draw an ellipse in the top of the canvas.right
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        
+        // Draw an ellipse in the bottom of the canvas.right
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+        
+        // Draw an ellipse in the top of the canvas.left
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 499 - x, centreY: 350, width: 50, height: 50)
+        
+        // Draw an ellipse in the bottom of the canvas.left
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: 499 - x, centreY: 150, width: 50, height: 50)
     }
     
 }
