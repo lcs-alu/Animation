5b412323498779c7d76b4cd854292194c6188dd4
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2017-11-03 11:14:19 -0400

    var

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -2,33 +2,19 @@ import Foundation
 
 class Sketch : NSObject {
     
-    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
-    //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Position of circle
-    var x : Int
-    
-    // This function runs once
-    override init() {
-        
+    // position of the circle
+    var x = 250
+    x = 100
+
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Set starting position
-        x = 250
+    
+        
+
         
     }
     
-    // Runs in a loop, forever, to create the animated effect
-    func draw() {
-        
-        // Change position
-        x += 1
-        
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
-    }
-    
-}
+
